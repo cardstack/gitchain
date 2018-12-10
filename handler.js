462cd795eab819b31b4422e30810f82247cc12b6
Alex Speller <alex@alexspeller.com>
2018-12-10 15:38:42 +0000

    Add hyperledger endpoint env config

--- handler.js
+++ handler.js
@@ -33,7 +33,7 @@ class GitchainHandler extends TransactionHandler {
   }
 }
 
-const address = 'tcp://127.0.0.1:4004';
+const address = process.env.HYPERLEDGER_ENDPOINT || 'tcp://127.0.0.1:4004';
 const transactionProcessor = new TransactionProcessor(address);
 transactionProcessor.addHandler(new GitchainHandler());
 transactionProcessor.start();
